7c1703b45fd65686ff62208d45a85319b8e01021
Momchil Buyukliev <113774637+momchil753@users.noreply.github.com>
2024-11-18 07:36:37 +0200

    Delete School Project Online Meet/School Project Online Meet directory

--- School Project Online Meet/School Project Online Meet/script.js
+++
@@ -1,7 +0,0 @@
-// Get the button:
-let mybutton = document.getElementById("myBtn");
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
